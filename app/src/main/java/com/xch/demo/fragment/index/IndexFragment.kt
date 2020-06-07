54f2600db34897ab6a5a64275afe59af155d2716
magiQAQ <39616939+magiQAQ@users.noreply.github.com>
2020-06-07 20:49:05 +0800

    学习中

--- app/src/main/java/com/xch/demo/fragment/index/IndexFragment.kt
+++ app/src/main/java/com/xch/demo/fragment/index/IndexFragment.kt
@@ -2,6 +2,8 @@ package com.xch.demo.fragment.index
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import com.xch.demo.R
 import com.xch.lib.fragments.bottom.BottomItemFragment
 import kotlinx.android.synthetic.main.fragment_index.*
@@ -13,5 +15,17 @@ class IndexFragment : BottomItemFragment(){
 
     override fun onBindView(savedInstanceState: Bundle?, rootView: View) {
 
+    }
+
+    //惰性的加载数据和UI
+    override fun onLazyInitView(savedInstanceState: Bundle?) {
+        super.onLazyInitView(savedInstanceState)
+        initRecycler()
+    }
+
+    private fun initRecycler() {
+        val manager = GridLayoutManager(context,4)
+        rv_index.layoutManager = manager
+        rv_index.addItemDecoration(DividerItemDecoration(context,DividerItemDecoration.VERTICAL))
     }
 }
--- lib/src/main/java/com/xch/lib/ui/recycler/MultipleRecyclerViewAdapter.kt
+++ lib/src/main/java/com/xch/lib/ui/recycler/MultipleRecyclerViewAdapter.kt
@@ -1,19 +1,29 @@
 package com.xch.lib.ui.recycler
 
-import android.content.ClipData
 import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.GridLayoutManager
+import com.blankj.utilcode.util.AdaptScreenUtils
+import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.listener.GridSpanSizeLookup
 import com.chad.library.adapter.base.listener.OnItemClickListener
+import com.xch.lib.R
+import com.xch.lib.ui.banner.ImageAdapter
+import com.youth.banner.Banner
+import com.youth.banner.indicator.CircleIndicator
 
 class MultipleRecyclerViewAdapter constructor(data: MutableList<MultipleItemEntity>) :
     BaseMultiItemQuickAdapter<MultipleItemEntity, MultipleViewHolder>(data),
     GridSpanSizeLookup,
-    OnItemClickListener{
+    OnItemClickListener {
+
+    //因为每次加载RecyclerView都会初始化变量(Banner实例)
+    //确保初始化一次
+    private var isInitBanner = false
 
     //初始化静态的参数和对象
     companion object {
@@ -22,30 +32,72 @@ class MultipleRecyclerViewAdapter constructor(data: MutableList<MultipleItemEnti
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .dontAnimate()
 
-        fun create(data: MutableList<MultipleItemEntity>):MultipleRecyclerViewAdapter{
+        fun create(data: MutableList<MultipleItemEntity>): MultipleRecyclerViewAdapter {
             return MultipleRecyclerViewAdapter(data)
         }
     }
 
     init {
-
+        initView()
     }
 
-    private fun initView(){
-        addItemType(ItemType.TEXT, TODO())
-        addItemType(ItemType.IMAGE, TODO())
-        addItemType(ItemType.TEXT_IMAGE, TODO())
-        addItemType(ItemType.BANNER, TODO())
+    private fun initView() {
+        addItemType(ItemType.TEXT, R.layout.item_multiple_text)
+        addItemType(ItemType.IMAGE, R.layout.item_multiple_image)
+        addItemType(ItemType.TEXT_IMAGE, R.layout.item_multiple_image_text)
+        addItemType(ItemType.BANNER, R.layout.item_multiple_banner)
+    }
+
+    override fun createBaseViewHolder(view: View): MultipleViewHolder {
+        return MultipleViewHolder.create(view)
     }
 
     //将数据转化成UI需要的方法
-    override fun convert(holder: MultipleViewHolder, item: MultipleItemEntity) {
-        TODO("Not yet implemented")
+    override fun convert(holder: MultipleViewHolder, entity: MultipleItemEntity) {
+        val text: String
+        val imageUrl: String
+        val bannerImages: ArrayList<String>
+        when (holder.itemViewType) {
+            ItemType.TEXT -> {
+                text = entity.getFields(MultipleFields.TEXT)
+                holder.setText(R.id.text_single, text)
+            }
+            ItemType.IMAGE -> {
+                imageUrl = entity.getFields(MultipleFields.IMAGE_URL)
+                Glide.with(context)
+                    .load(imageUrl)
+                    .apply(RECYCLER_OPTIONS)
+                    .into(holder.getView(R.id.image_single) as AppCompatImageView)
+            }
+            ItemType.TEXT_IMAGE -> {
+                text = entity.getFields(MultipleFields.TEXT)
+                imageUrl = entity.getFields(MultipleFields.IMAGE_URL)
+                holder.setText(R.id.text_multiple, text)
+                Glide.with(context)
+                    .load(imageUrl)
+                    .apply(RECYCLER_OPTIONS)
+                    .into(holder.getView<AppCompatImageView>(R.id.image_multiple))
+            }
+            ItemType.BANNER -> if (!isInitBanner) {
+                bannerImages = entity.getFields(MultipleFields.BANNERS)
+                val banner = holder.getView<Banner<String, ImageAdapter>>(R.id.banner_recycler_item)
+                banner.setAdapter(ImageAdapter(context, bannerImages))
+                    .setBannerRound(AdaptScreenUtils.pt2Px(5f).toFloat()).indicator =
+                    CircleIndicator(context)
+                isInitBanner = true
+            }
+            else -> {
+            }
+        }
     }
 
     //根据SpanSize来判断多布局的分割
-    override fun getSpanSize(gridLayoutManager: GridLayoutManager, viewType: Int, position: Int): Int{
-        TODO("adadadad")
+    override fun getSpanSize(
+        gridLayoutManager: GridLayoutManager,
+        viewType: Int,
+        position: Int
+    ): Int {
+        return data[position].getFields(MultipleFields.SPAN_SIZE)
     }
 
     //轮播图的点击事件
